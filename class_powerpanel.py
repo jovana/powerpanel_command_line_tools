0b98efdaafc8f3cab4bcecf20be6bdbf8eceef19
Joël <joel@jovana.nl>
2018-10-04 20:58:14 +0200

    Updates

--- class_powerpanel.py
+++ class_powerpanel.py
@@ -5,6 +5,9 @@
 
 import json
 import requests
+import colors
+import os
+from pathlib import Path
 
 class PowerPanel:
     """ PowerPanel Class. Use this class in your Python app.
@@ -32,9 +35,45 @@ class PowerPanel:
                 url = "https://api.test.powerpanel.io/1.0/" + p_RestCommand,
                 headers = {
                     "API_KEY": self.api_key,
+                    #"cu_id": "1421",
                     "Content-Type": "application/json",
                     "ASYNC": "True"
                 },
                 data = p_JsonData
             )
         return response
+
+    def loadListFromDisk(self, p_FileName):
+        """ Loading data list needed for the bulk action.
+
+        Args:
+            param1 (str): The filename to open and read content.
+
+        Returns:
+            object: open().readLines() output
+        """
+
+        file_path = Path(os.path.abspath(os.path.join(os.path.dirname(__file__), p_FileName)))
+        if not file_path.is_file():
+            print(colors.bcolors.FAIL + 'File name ' + p_FileName + ' not found!' + colors.bcolors.ENDC)
+            print('For this action the the file with name: ' + p_FileName + ' is needed. Make sure you have created this file and add the content to.')
+            self.file_contents = None
+            return False
+        else:
+            print(colors.bcolors.WARNING + """
+            Do you have added / update the correct content into the file: """ + p_FileName + """? [Default = N]
+            Y = Yes
+            N = No
+            """+ colors.bcolors.ENDC) 
+            file_updated = input("Please select Y or N: ") 
+            if file_updated == 'Y':
+                self.file_contents = open(p_FileName,"r").readlines()
+                return True
+            else:
+                print(colors.bcolors.FAIL + 'Wrong or unsupported input given: ' + file_updated+ colors.bcolors.ENDC)
+                self.file_contents = None
+                return False
+
+        
+            
+        
--- start.py
+++ start.py
@@ -6,22 +6,19 @@ import os
 from collections import namedtuple
 from pathlib import Path
 
-CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config.json'))
-config_file = Path(CONFIG_PATH)
+# loading config file
+config_file = Path(os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.json')))
 
+# Check if config files has found, if not print error and exit
 if not config_file.is_file():
     print(colors.bcolors.FAIL + 'config.json not found!' + colors.bcolors.ENDC)
     print('Make sure you have a config file named config.json. This file contains also your API key.')
     exit()
 
-CONFIG = json.load(open(CONFIG_PATH), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-
-domain_file = open("domain_list.txt","r")
-domain_file_contents = domain_file.readlines()
-api_key = CONFIG.api_key
+# load config settings from config.json
+config = json.load(open(config_file), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+api_key = config.api_key
 selection = True
-
-#print( entry, menu[entry])
 
 while selection: 
 
@@ -39,36 +36,45 @@ while selection:
         4. Exit / Quit
         """ + colors.bcolors.ENDC)
         
-    selection = input("Please Select:") 
+    selection = input("Please choose the number for the action you want to execute: ") 
+
+    # init PowerPanel Class
+    oPowerPanel = class_powerpanel.PowerPanel(api_key)
 
     if selection =='1':
         # Running domain info for the domains in file domain_list.txt
 
+        # Ask if domain expireddate needs to update
         print(colors.bcolors.WARNING + """
-            Do you want to udate the domain expiredate? [Default = N]
-            Y = Yes
-            N = No
+        Do you want to udate the domain expiredate? [Default = N]
+        Y = Yes
+        N = No
         """+ colors.bcolors.ENDC) 
-        update_expiredate = input("Please Select Y or N:") 
+        update_expiredate = input("Please Select Y or N: ")
 
-        for domain in domain_file_contents:            
-            print('Running getDomainInfo for domain: ' +domain)
-            data = {}
-            data["domainname"] = domain
-            data['live'] = True
-            if update_expiredate == 'Y':
-                data['update_subscription_date'] = True
+        # clear screen 
+        os.system( 'clear' )  
 
-            oPowerPanel = class_powerpanel.PowerPanel(api_key)
-            response = oPowerPanel.apiCommand("Domain/info", json.dumps(data))
-            response_json = json.loads(response.content.decode('UTF-8'))
+        if oPowerPanel.loadListFromDisk("domain_list.txt"):
+            for domain in oPowerPanel.file_contents:                       
+                print('Running getDomainInfo for domain: ' +domain)
+                data = {}
+                data["domainname"] = domain
+                data['live'] = True
+                if update_expiredate == 'Y':
+                    data['update_subscription_date'] = True
+                
+                # Set API command
+                response = oPowerPanel.apiCommand("Domain/info", json.dumps(data))
+                response_json = json.loads(response.content.decode('UTF-8'))
 
-            # print status from action
-            if response_json['code'] == 1:
-                print(colors.bcolors.OKGREEN + 'Status: SUCCESS!\n' + response_json['msg'][0] + colors.bcolors.ENDC)
-            else:
-                print(colors.bcolors.FAIL + 'Status: FAIL: ' +  response_json['msg'][0] + colors.bcolors.ENDC)
-            print('========================================')    
+                # print status from action
+                if response_json['code'] == 1:
+                    print(colors.bcolors.OKGREEN + 'Status: SUCCESS!\n' + response_json['msg'][0] + colors.bcolors.ENDC)
+                else:
+                    print(colors.bcolors.FAIL + 'Status: FAIL: ' +  response_json['msg'][0] + colors.bcolors.ENDC)
+                print('========================================')
+            print('Task done, returning to the menu...') 
 
     elif selection == '2':
         print( "delete")
